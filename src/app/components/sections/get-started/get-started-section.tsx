fa6da8589c201cb721308f47c4502541a00235ab
DruMoDev <dev.morera@gmail.com>
2025-01-27 13:45:14 +0100

    Refactor Get Started section layout for improved responsiveness and alignment

--- src/app/components/sections/get-started/get-started-section.tsx
+++ src/app/components/sections/get-started/get-started-section.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function GetStartedSection() {
   return (
-    <section className="pt-20 bg-primary-light relative overflow-hidden">
-      <div className="grid grid-cols-2 pl-20">
-        <div className="flex flex-col gap-5">
+    <section className="py-20 bg-primary-light relative overflow-hidden">
+      <div className="grid grid-cols-2 max-w-[1308px] mx-auto">
+        <div className="flex flex-col gap-5 pr-20">
           <ImpakterProTextLogo />
           <h2 className="h2-content">Ready to get started?</h2>
           <p className="p-content font-semibold">
@@ -17,7 +17,8 @@ export default function GetStartedSection() {
             <button className="btn btn-secondary">Get Free Score</button>
           </div>
         </div>
-        <div>
+      </div>
+        <div className="absolute bottom-0 right-0 w-1/2 max-h-[90%]">
           <Image
             src={"/get-started-img.webp"}
             width={1316}
@@ -25,7 +26,6 @@ export default function GetStartedSection() {
             alt="Dashboard image"
           />
         </div>
-      </div>
     </section>
   );
 }
